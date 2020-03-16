d0393a8c39ddd72ec0c07caf6bcbb7d9dec77b96
Steven Williams <evilgeniusnyc@me.com>
2020-03-16 16:53:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,18 +1,28 @@
 def roll_call_dwarves(array)
  array.each_with_index do |dwarf, index|
+   if index => array.length /2
+     break
+   end
    puts "#{index + 1}.#{dwarf}"
  end
 end
 
-def summon_captain_planet(array)
-    array.collect { |i| i.capitalize << "!" } 
- end	
 
+
+
+def summon_captain_planet(array)
+  array.collect { |element| element.capitalize << "!" } 
+end	
 
 def long_planeteer_calls(calls)
-     calls.any? { |i| i.length > 4 }
+  calls.any? { |word| word.length > 4 }
 end
 
 def find_the_cheese(array)
-array.find { |i| i == "cheddar" || i == "gouda" || i == "camembert" }
+  array.find { |i| i == "cheddar" || i == "gouda" || i == "camembert" }
 end
+
+
+
+
+
